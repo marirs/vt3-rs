971fa8ee938cab44c383b0fa51e10bcebe3e1fc0
Sriram Govindan <marirs@gmail.com>
2021-06-15 21:17:39 +0530

    url feeds

--- src/enterprise/mod.rs
+++ src/enterprise/mod.rs
@@ -1,3 +1,4 @@
 pub(crate) mod file_feeds;
 pub(crate) mod livehunt;
 pub(crate) mod retrohunt;
+pub(crate) mod url_feeds;
---
+++ src/enterprise/url_feeds/mod.rs
@@ -0,0 +1,38 @@
+mod response;
+use response::Root;
+
+use crate::{utils::http_get_bz, VtClient, VtResult};
+
+impl VtClient {
+    pub fn url_feed(self, time: &str) -> VtResult<Vec<Root>> {
+        //! Get a URL feed batch.
+        //!
+        //! ## Example Usage
+        //! ```rust
+        //! use vt3::VtClient;
+        //!
+        //! let vt = VtClient::new("Your API Key");
+        //!
+        //! // A string in format YYYYMMDDhhmm
+        //! vt.url_feed("202106131355");
+        //! ```
+        let url = format!("{}/feeds/urls/{}", self.endpoint, time);
+        http_get_bz(&self.api_key, &self.user_agent, &url)
+    }
+
+    pub fn url_feed_hourly(self, time: &str) -> VtResult<Vec<Root>> {
+        //! Hourly file feed batch.
+        //!
+        //! ## Example Usage
+        //! ```rust
+        //! use vt3::VtClient;
+        //!
+        //! let vt = VtClient::new("Your API Key");
+        //!
+        //! // A string in format YYYYMMDDhh
+        //! vt.url_feed("2021061313");
+        //! ```
+        let url = format!("{}/feeds/urls/hourly/{}", self.endpoint, time);
+        http_get_bz(&self.api_key, &self.user_agent, &url)
+    }
+}
