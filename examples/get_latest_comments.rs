6c841951535fa03cf5d17833eab93209a06dbf99
Sriram Govindan <marirs@gmail.com>
2021-06-15 13:22:27 +0530

    example update

--- examples/get_latest_comments.rs
+++ examples/get_latest_comments.rs
@@ -9,7 +9,7 @@ fn main() {
         }
     };
 
-    let res = VtClient::new(&api_key).get_comments(Some("10"), Some("tag%3Amalware"), None);
+    let res = VtClient::new(&api_key).get_comments(Some("10"), Some("tag:malware"), None);
     match res {
         Ok(report) => println!("{:#?}", report),
         Err(e) => println!("Error: {}", e.to_string()),
