148b6f32e2626005ec3352cac1e9338ab27e9baf
Sriram Govindan <marirs@gmail.com>
2021-06-15 21:17:18 +0530

    file feed hourly

--- src/enterprise/file_feeds/mod.rs
+++ src/enterprise/file_feeds/mod.rs
@@ -8,7 +8,7 @@ impl VtClient {
         //! Get a file feed batch.
         //!
         //! ## Example Usage
-        //! ```
+        //! ```rust
         //! use vt3::VtClient;
         //!
         //! let vt = VtClient::new("Your API Key");
@@ -17,6 +17,22 @@ impl VtClient {
         //! vt.file_feed("202106131355");
         //! ```
         let url = format!("{}/feeds/files/{}", self.endpoint, time);
+        http_get_bz(&self.api_key, &self.user_agent, &url)
+    }
+
+    pub fn file_feed_hourly(self, time: &str) -> VtResult<Vec<Root>> {
+        //! Hourly file feed packages.
+        //!
+        //! ## Example Usage
+        //! ```rust
+        //! use vt3::VtClient;
+        //!
+        //! let vt = VtClient::new("Your API Key");
+        //!
+        //! // A string in format YYYYMMDDhh
+        //! vt.file_feed("2021061313");
+        //! ```
+        let url = format!("{}/feeds/files/hourly/{}", self.endpoint, time);
         http_get_bz(&self.api_key, &self.user_agent, &url)
     }
 }
