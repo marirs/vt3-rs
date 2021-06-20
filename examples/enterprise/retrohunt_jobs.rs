d88696b25bcb5157269fb118be82f20e17576814
Sriram Govindan <marirs@gmail.com>
2021-06-20 10:03:36 +0530

    more examples

---
+++ examples/enterprise/retrohunt_jobs.rs
@@ -0,0 +1,22 @@
+use vt3::VtClient;
+
+fn main() {
+    let mut args = std::env::args().skip(1);
+    let api_key = match args
+        .next()
+        .ok_or("Please provide the api key as argument!")
+    {
+        Ok(api_key) => api_key,
+        Err(e) => {
+            println!("{:?}", e);
+            std::process::exit(1)
+        }
+    };
+
+    let res = VtClient::new(&api_key)
+        .get_retrohunt_jobs(None, None, None);
+    match res {
+        Ok(report) => println!("{:#?}", report),
+        Err(e) => println!("Error: {}", e.to_string()),
+    }
+}
